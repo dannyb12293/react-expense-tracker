22b0d9f2515c85bcc98b87f646d9de9f719b6057
Daniel Burgos <dannyb12293@yahoo.com>
2022-07-21 20:58:46 -0400

    Created Card.js

--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,12 +1,13 @@
 import React from "react"
 import ExpenseItem from "./ExpenseItem"
+import Card from "./Card"
 import './Expense.css'
 
 function Expenses (props) {
    
     
   return (
-    <div className="expenses">
+    <Card className="expenses">
        <ExpenseItem 
       title={props.items[0].title} 
       amount={props.items[0].amount} 
@@ -27,7 +28,7 @@ function Expenses (props) {
       amount={props.items[3].amount} 
       date={props.items[3].date}
       />
-    </div>
+    </Card>
     
   )
   }
